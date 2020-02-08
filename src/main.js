79ed54bae1f8a85cafef25816d6457ed9d678ea4
shentao <damian.dulisz@gmail.com>
2020-02-08 21:51:23 +0100

    Vue-next + tailwind setup

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import './assets/tailwind.css'
 
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+createApp(App).mount('#app')
